a9ef9ca77c266ef8ae5641dfac8b063bb994630f
Walter Fritsche <wfritsche95@protonmail.com>
2021-05-04 15:41:12 -0500

    init setup with webpack, babel, sass, postcss webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+let mode = 'development';
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    target = "browserlist";
+}
+
+module.exports = {
+    mode: mode,
+    target: target,
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: "images/[hash][ext][query]",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpe?g|webp|gif)$/i,
+                type: "asset",
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    devtool: "source-map",
+    devServer: {
+        contentBase: "./dist",
+        hot: true,
+        host: "0.0.0.0",
+    }
+}
