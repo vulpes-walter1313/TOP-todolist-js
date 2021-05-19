1612fa576456f6afe109b3920b89a07f9547142c
Walter Fritsche <wfritsche95@protonmail.com>
2021-05-19 16:03:07 -0500

    cleaned up to have todos in modules.

--- src/index.js
+++ src/index.js
@@ -11,9 +11,3 @@ todos.push(todoFactory('Walk', 'walk the doggo', 2));
 todos.push(todoFactory('Study', 'do the thing with the books', 1));
 
 paintUI();
-
-const todosContainer = document.querySelector('.todos-list-container');
-todos.forEach(todo=> {
-    let todoComp = createTodoComponent(todo.getTitle());
-    todosContainer.appendChild(todoComp);
-});
