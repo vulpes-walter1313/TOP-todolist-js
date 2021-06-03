6074e689cd81c9758ad24e7eff8672f907a12bd7
Walter Fritsche <wfritsche95@protonmail.com>
2021-06-03 16:42:23 -0500

    cleaned it up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import './css/index.scss';
 import { paintUI } from './js/mainUI';
-import todoFactory from './js/todoFactory';
-import createTodoComponent from './js/todoComponent';
-
-
-
-let todos = [];
-todos.push(todoFactory('Eat', 'make Spaget and eat', 3));
-todos.push(todoFactory('Walk', 'walk the doggo', 2));
-todos.push(todoFactory('Study', 'do the thing with the books', 1));
 
 paintUI();
